0f8332eebf50474e7b9e29e5939b7cead90b8525
estebanvilla1024 <138405538+estebanvilla1024@users.noreply.github.com>
2023-07-11 11:58:05 -0500

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+
+#define usuario1="Andrea"
+#define usuario1="Juan"
+#define usuario1="Esteban"
+
+#define salaNormalAsientos = "36"
+#define salaVipAsientos = "10"
+#define sala3DAsientos = "36"
+
+#define pelicula1 = "1"
+#define pelicula2 = "2"
+
+#define valorEntradaNormal = "$8.00"
+#define valorEntradaVip = "$15.00"
+#define valorEntradatresD = "$12.00"
+int main ()
+{
+
+      char usuario, password;
+       char usuario1[10], "Andrea";
+        char usuario2[10], "Juan" 
+         char usuario3[10], "Esteban";
+         int password1[4] == "4321";
+         int password2[4] == "3478";
+         int password3[4] == "9987";
+
+  printf ("Ingrese su usuario: ");
+  scanf ("%d", usuario);
+  
+  if (usuario == usuario1)
+    {
+        printf ("usuario correcto, ingrese su contraseña del usuario 1: ");
+        scanf ("%s", contraseña1&&);}
+    else {
+        if (usuario== usuario2){
+             printf ("usuario correcto, ingrese su contraseña del usuario 2: ");
+             scanf ("%s", contraseña2&&) 
+        }
+        if (usuario==usuario3){
+             printf ("usuario correcto, ingrese su contraseña del usuario 3: ");
+             scanf ("%s", contraseña3&&) 
+        }
+    }
+    
+    switch
+     {
+        case 1: 
+
+     char ventaEntrada, sala, normal, vip, tresD;
+     float valorEntradaNormal, valorEntradaVip, valorEntradatresD;
+     float pelicula1, peicula2, entradas;
+      printf("eliga en que sala desesa ver su pelìcula: ");
+      scanf("%c", sala);
+      printf("cuantas entradas desea comprar: ");
+      scanf("%f", &&entradas);
+      
+      printf("Ingrese la película que desea ver entre: pelicula1 y pelicula2: ");
+      scanf("%f", pelicula);
+      if (sala == normal) {
+        printf("el valor de de la entrada es: ");
+        scanf("%c", &&valorEntradaNormal);
+
+      }
+      else 
+      {
+        if (sala == vip)
+         {
+        printf("el valor de de la entrada es: ");
+        scanf("%c", &&valorEntradaVip);
+        }
+
+        if (sala == tresD)
+         {
+        printf("el valor de de la entrada es: ");
+        scanf("%c", &&valorEntradatresD);
+        }
+
+      }
+
+
+     case 2:
+     char estadisticaVenta, totalCompra, descuento, 
+     do (estadisticaVenta= ([entradas*valorEntrada]*descuento)
+     {
+        float descuento= descuento*
+
+
+     }
+     return 1;
+     }
+ return 0;
+}
